6328f2984f5f3c7f1df7b1ab7bf7dfd5453c2295
Alpq <doymazalp@gmail.com>
2021-01-14 08:31:34 -0500

    Added the radixSortSimple() function and working on testing. File compiles.

--- Radix.java
+++ Radix.java
@@ -1,16 +1,37 @@
 public class Radix{
   public static int nth(int n, int col)
   {
-    return n % Math.pow(10, 1 + col);
+    return n % (int)Math.pow(10, 1 + col);
   }
   public static int length(int n)
   {
-    return Math.floor(Math.log(n));
+    return (int)Math.floor(Math.log(n));
   }
-  public static void merge(MyLinkedList original,MyLinkedList[]buckets)
+  public static void merge( SortableLinkedList original, SortableLinkedList[]buckets)
   {
     for (int i =0 ; i < buckets.length ; i ++ ) {
-      buckets[i].extend(buckets[i + 1])
+      original.extend(buckets[i]);
     }
   }
+  public static void radixSort(SortableLinkedList data)
+  {
+  }
+  public static void radixSortSimple(SortableLinkedList data)
+  {
+    int basesize = 0;
+    for (int i = 0; i < data.size() ; i ++ ) {
+      basesize = Math.max(data.get(i), basesize);
+    }
+    for (int i = 0; i < basesize ; i ++ ) {
+      SortableLinkedList[] bucket = new SortableLinkedList[10];
+      for (int j = 0; j < data.size(); j += 0)
+      {
+        int digit = nth(data.get(0), i);
+        bucket[digit].add(data.get(0));
+        data.remove(0);
+      }
+      merge(data, bucket);
+    }
+  }
+
 }
