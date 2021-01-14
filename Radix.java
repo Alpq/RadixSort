f5135b8f7ed0f707cecade8572fb8421b255359d
Alpq <doymazalp@gmail.com>
2021-01-14 08:44:16 -0500

    Added helper method that sorted linked list given a specific base, and made a for loop in Radixsort that called on that helper method for each base necessary.

--- Radix.java
+++ Radix.java
@@ -19,19 +19,25 @@ public class Radix{
   public static void radixSortSimple(SortableLinkedList data)
   {
     int basesize = 0;
-    for (int i = 0; i < data.size() ; i ++ ) {
+    for (int i = 0; i < data.size() ; i ++ )
+    {
       basesize = Math.max(data.get(i), basesize);
     }
-    for (int i = 0; i < basesize ; i ++ ) {
-      SortableLinkedList[] bucket = new SortableLinkedList[10];
-      for (int j = 0; j < data.size(); j += 0)
-      {
-        int digit = nth(data.get(0), i);
-        bucket[digit].add(data.get(0));
-        data.remove(0);
-      }
-      merge(data, bucket);
+    for (int i = 0; i < basesize ; i ++ )
+    {
+      radixSortSimplest(data, i);
     }
   }
-
+  public static void radixSortSimplest(SortableLinkedList data, int base)
+  {
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i ++ ) { buckets[i] = new SortableLinkedList();}
+    while (data.size() > 0)
+    {
+      int digit = nth(data.get(0), base);
+      buckets[digit - 1].add(data.get(0));
+      data.remove(0);
+    }
+    merge(data, buckets);
+  }
 }
